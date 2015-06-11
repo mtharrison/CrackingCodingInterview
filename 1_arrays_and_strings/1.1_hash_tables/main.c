355f625098945e72b2624edc536b540b8e8a8ec9
Matt Harrison <hi@matt-harrison.com>
2015-06-12 01:36:15 +0800

    Bin style hashmap

--- 1_arrays_and_strings/1.1_hash_tables/main.c
+++ 1_arrays_and_strings/1.1_hash_tables/main.c
@@ -14,27 +14,27 @@ int main(int argc, char const *argv[])
 {
 	// HashmapSimple
 
-    // HashmapSimple* hashmap = HashmapSimpleNew();
+    HashmapSimple* hashmap = HashmapSimpleNew();
     
-    // HashmapSimpleSet(hashmap, "First Name", "Matt");
-    // HashmapSimpleSet(hashmap, "Last Name", "Harrison");
+    HashmapSimpleSet(hashmap, "First Name", "Matt");
+    HashmapSimpleSet(hashmap, "Last Name", "Harrison");
 
-    // printf("%s\n", HashmapSimpleGet(hashmap, "First Name"));
-    // printf("%s\n", HashmapSimpleGet(hashmap, "Last Name"));
+    printf("%s\n", HashmapSimpleGet(hashmap, "First Name"));
+    printf("%s\n", HashmapSimpleGet(hashmap, "Last Name"));
 
-    // free(hashmap);
+    free(hashmap);
 
-    // // HashmapLinkedList
+    // HashmapLinkedList
 
-    // HashmapLinkedList* hashmap2 = HashmapLinkedListNew();
+    HashmapLinkedList* hashmap2 = HashmapLinkedListNew();
 
-    // HashmapLinkedListSet(hashmap2, "First Name", "Matt");
-    // HashmapLinkedListSet(hashmap2, "Last Name", "Harrison");
+    HashmapLinkedListSet(hashmap2, "First Name", "Matt");
+    HashmapLinkedListSet(hashmap2, "Last Name", "Harrison");
 
-    // printf("%s\n", HashmapLinkedListGet(hashmap2, "First Name"));
-    // printf("%s\n", HashmapLinkedListGet(hashmap2, "Last Name"));
+    printf("%s\n", HashmapLinkedListGet(hashmap2, "First Name"));
+    printf("%s\n", HashmapLinkedListGet(hashmap2, "Last Name"));
 
-    // free(hashmap2);
+    free(hashmap2);
 
     // HashmapFast
 
